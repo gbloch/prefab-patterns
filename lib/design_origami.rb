85d9a6bc61b8fc4730b411508892d9d43d7006f9
Jason Ramirez <ramirez676@gmail.com>
2015-06-27 09:00:35 -0400

    New file soure for patters
    File source is now: jasonramirez/design-takeout

--- lib/design_origami.rb
+++ lib/design_origami.rb
@@ -3,7 +3,7 @@ require "design_origami/render_pattern"
 require "design_origami/import_pattern"
 require "design_origami/railtie" if defined?(Rails)
 
-FILE_PATH = "https://raw.githubusercontent.com/jasonramirez/design-origami-marketplace/master/app/patterns/"
+FILE_PATH = "https://raw.githubusercontent.com/jasonramirez/design-takeout/master/"
 
 module DesignOrigami
 end
